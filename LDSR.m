8b7117967d17a2bbe26ddd81f88f10b994b9515d
kailigo <li.kai.gml@gmail.com>
2018-12-27 21:15:43 -0500

    clean the code

--- LDSR.m
+++ LDSR.m
@@ -3,7 +3,6 @@ function [AUC] = LDSR( X, label, outLabel, ...
 
 nv = length(X);
 
-
 max_mu = 1e10;
 mu = 1e-4;
 rho = 1.3;
@@ -27,16 +26,8 @@ XtX = cell(nv, 1);
 E = cell(nv,1);
 Ze = cell(nv, 1);
 sumXtX = 0;
-sumWtx = 0;
-W = cell(nv, 1);
 for n =  1:nv
   x_dims(n) = size(X{n},1); 
-end
-md = min(x_dims);
-
-unq_dim = 1;
-if length(unique(x_dims)) ~= 1
-    unq_dim = 0;
 end
 
 for n = 1:nv
@@ -45,26 +36,12 @@ for n = 1:nv
         X{n}(:,i) = X{n}(:,i) ./ norm(X{n}(:,i));
     end
   end
-%   W{n} = orth(X{n}');
-%   XW{n} = X{n} * W{n}(:, 1:md);
-  
-%   Ze{n} = zeros(ns, x_dims(n));
   Ze{n} = zeros(ns, ns);
-   
-%   wtX{n} = XW{n} * X{n};
-%   sumWtx = sumWtx + wtX{n};
- 
   XtX{n} = X{n}'*X{n};
   sumXtX = sumXtX + XtX{n};
   E{n} = sparse(x_dims(n), ns);
   Q{n} = zeros(x_dims(n), ns);
 end
-% 
-
-% rng(0);
-
-% P = rand(ns, ns);
-% Zc = rand(ns, ns);
 
 P = zeros(ns, ns);
 Zc = zeros(ns, ns);
@@ -88,19 +65,6 @@ while iter<maxIter
         sigma = 0;
     end
     J = U(:,1:svp)*diag(sigma)*V(:,1:svp)';
-     
-%     temp1 = zeros(size(Ze{1}));
-%     temp1 = zeros(size(wtX{1}));
-%     temp2 = temp1;
-%     temp3 = temp1;
-%     for i = 1:nv
-%        temp1 = temp1 + wtX{i} * Ze{i};
-%        temp2 = temp2 + XW{i}' * E{i};
-%        temp3 = temp3 + XW{i}' * Q{i};
-%     end
-%     temp4 = -P+mu*J+mu*sumWtx-mu*temp1-mu*temp2+temp3;
-%     temp5 = mu*eye(ns)+mu*sumWtX;
-%     Zc = temp4 \ temp5;
   
     temp1 = zeros(size(Ze{1}));
     temp2 = temp1;
@@ -116,13 +80,10 @@ while iter<maxIter
     
     break_flag = 0;
     for i = 1:nv
-
       r = 0.5 ./ sqrt(diag(Ze{i}*Ze{i}')+epi);
-%        r = 0.5 ./ sqrt(diag(Ze{i}'*Ze{i})+epi);    
       R = diag(r);
       temp1 = mu*X{i}'*(X{i}*Zc+E{i}-X{i}-Q{i}/mu);
       temp2 = 2*alpha*R+mu*X{i}'*X{i};  
-%       temp2 = alpha*eye(n)+mu*X{i}'*X{i};
       Ze{i} = - temp2 \ temp1;
       
       %update Ev
@@ -142,57 +103,32 @@ while iter<maxIter
     %update P
     leq2 = Zc - J;
     P = P + leq2 * mu;
-
-%      fprintf("leq1: %f \n", max(max(abs(leq1))));
-%     disp('leq1: ');
-%     disp();
-%      fprintf("leq2: %f \n", max(max(abs(leq2))));
-    
-%     disp('leq2: ');
-%     disp(max(max(abs(leq2))));
-   err1 = norm(leq1, 'fro') / norm(X{i}, 'fro');
-    err2 = norm(leq2, 'fro') / norm(X{i}, 'fro');
-    err(iter) = max([err1, err2]);
     
     if break_flag && max(max(abs(leq2))) < tol
-%        disp("break has been executed");
        break;
     end
-    mu = min(max_mu,mu*rho);
-    
-    for i = 1:nv
-        Z{i} = Ublk .* (Zc + Ze{i});
-%         Z{i} = Ublk .* (Zc + Ze{i});
-%        Z{i} = Zc + Ze{i};
-    end
-    
-    err2_flag = 0;
-    if any(any(E{i}))
-       err2_flag = 1;
-    end
-        
-    for i = 1:ns
-      err1 = 0;
-      err2 = 0;
-      for j = 1:nv
-        err1 = err1 + norm(Ze{j}(:, i));
-        if any(E{j}(:, i))
-           err2 = err2 + norm(E{j}(:, i));
-        else
-           err2 = 0;  
-        end
-      end
-      consistency(i) = -err1 - lambda1 * err2;  
-    end
-   
-    if length(consistency) > 1
-        [~, ~, ~, AUC] = perfcurve(outLabel, consistency, 0);
-    end
-    
-
-    AUCS(iter) = AUC;
-
+    mu = min(max_mu,mu*rho);   
 end
+
+
+for i = 1:ns
+  err1 = 0;
+  err2 = 0;
+  for j = 1:nv
+    err1 = err1 + norm(Ze{j}(:, i));
+    if any(E{j}(:, i))
+       err2 = err2 + norm(E{j}(:, i));
+    else
+       err2 = 0;  
+    end
+  end
+  consistency(i) = -err1 - lambda1 * err2;  
+end
+
+if length(consistency) > 1
+    [~, ~, ~, AUC] = perfcurve(outLabel, consistency, 0);
+end
+  
 disp(AUC);
 
 end
